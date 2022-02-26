3e3f8719e362ff3b73210eef94d1b4a2f56eb134
DavidGelmanSerg <gelmands@college.omsu.ru>
2022-02-26 20:54:01 +0600

    third task

--- js/GetGoods.js
+++ js/GetGoods.js
@@ -1,29 +1,69 @@
 const getgoods = () => {
 
-    //функция получения данных с сервера----------------------------------------------
-    const getdata = () => {
-        fetch('/db/db.json')//запрос к серверу(файлу)
-            .then((res) => res.json())//получаем данные и преобразуем
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = '';
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+
+            goodBlock.innerHTML = `
+                    <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span> 
+
+						<img src="db/${good.img}" alt="${good.name}" class="goods-image">
+
+						<h3 class="goods-title">${good.name}</h3>
+
+						<p class="goods-description">${good.description}</p>
+
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+
+							<span class="button-price">$${good.price}</span>
+
+						</button>
+					</div>
+            `
+            goodsContainer.append(goodBlock);
+        })
+    }
+
+    const getdata = (value, category) => {
+        fetch('/db/db.json')
+            .then((res) => res.json())
             .then((data) => {
-                localStorage.setItem('goods', JSON.stringify(data));
+                const array = category ? data.filter((item) => item[category] === value) : data;
+                localStorage.setItem('goods', JSON.stringify(array));
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array);
+                }
             })
     }
-    //обработка событий при клике на каждую ссылку-----------------------------------
-    const links = document.querySelectorAll('.navigation-link');//получаем ссылки на товары   
+    //MAIN---------------------------------------------------------------------------------------------------------------------------------   
+    const links = document.querySelectorAll('.navigation-link');
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
-            getdata();
+
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+            getdata(linkValue, category);
         })
     })
+    if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
 
-
-    /*localStorage.setItem('goods', JSON.stringify({ name: 'all' }));//запись элемента хранилище
-
-    const goods = (JSON.parse(localStorage.getItem('goods')));//получение элемента из хранилища
-
-    localStorage.removeItem('goods');*///удаление элемента из хранилища
-
+    const more = document.querySelector('.more');
+    more.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = '/goods.html';
+    })
 }
 
 getgoods() 
--- js/search.js
+++ js/search.js
@@ -4,9 +4,62 @@ const search = function () {
     /*input.addEventListener('input', (event) => {
         console.log(event.target.value);
     })*/
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');//получаем класс карточек
+
+        goodsContainer.innerHTML = '';//очищаем страницу
+
+        goods.forEach(good => {
+            const goodBlock = document.createElement('div');//создаем новый блок div
+            goodBlock.classList.add('col-lg-3');//добавляем новый класс к блоку
+            goodBlock.classList.add('col-sm-6');//добавляем новый класс к блоку
+            //вставляем карточки на страницу с помощью верстки------------------------------------
+            goodBlock.innerHTML = `
+                    <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span> 
+
+						<img src="db/${good.img}" alt="${good.name}" class="goods-image">
+
+						<h3 class="goods-title">${good.name}</h3>
+
+						<p class="goods-description">${good.description}</p>
+
+						<button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+
+							<span class="button-price">$${good.price}</span>
+
+						</button>
+					</div>
+            `
+            goodsContainer.append(goodBlock);//доблавяем в конец страницы готовый созданный div---
+        })
+    }
+
+    //функция получения данных с сервера----------------------------------------------------------
+    const getdata = (value) => {
+        fetch('/db/db.json')//запрос к серверу(файлу)
+            .then((res) => res.json())//получаем данные и преобразуем
+            .then((data) => {
+                const array = data.filter(good => {
+
+                    return good.name.toLowerCase().includes(value.toLowerCase());//ищем карточки по значению инпута
+                })
+
+                localStorage.setItem('goods', JSON.stringify(array));//записываем в локальное хранилище отфильтрованный массив данных                           
+
+                if (window.location.pathname !== '/goods.html') { //условие для перехода на страницу с товарами
+                    window.location.href = '/goods.html';// переход на страницу с товарами
+                } else {
+                    renderGoods(array);//рендер товаров
+                }
+            })
+    }
+
     searchBtn.addEventListener('click', () => {
-        console.log(input.value);
+        getdata(input.value);
     })
+
+
 }
 
 search();
